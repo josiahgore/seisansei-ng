02642d69b7ea9eea67a7701fb4c7964763ea2935
Josiah Gore <josiahgore@gmail.com>
2014-04-03 15:44:18 -0400

    project skeleton generated via yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('seisanseiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
